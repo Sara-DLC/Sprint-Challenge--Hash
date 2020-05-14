fa75321cfb3bf282c24851013314180d4670d28c
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-05-13 23:28:14 -0500

    ex3 and ex4 completed

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,17 @@
 def intersection(arrays):
-
-    """
-    YOUR CODE HERE
-    """
+    # start empty result list and empty dictionary
+    result = []
+    d = {}
+    # iterated through array and append keys
+    for i in arrays:
+        for j in i:
+            if j in d:
+                d[j] += 1
+            else:
+                d[j] = 1
+    for key in d.keys():
+        if d[key] == len(arrays):
+            result.append(key)
 
     return result
 
@@ -10,8 +19,8 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,21 @@
 def has_negatives(a):
-
-    """
-    YOUR CODE HERE
-    """
+    # set empty result list, negative and positive empty dict
+    result = []
+    negative = {}
+    positive = {}
+    # iterate through items and assign to positive or negative dict
+    for i in a:
+        if i < 0:
+            negative[i] = i*-1
+        else:
+            positive[i] = i
+    # iterated through and append positive items
+    for i in negative.values():
+        if i in positive:
+            result.append(positive[i])
 
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
