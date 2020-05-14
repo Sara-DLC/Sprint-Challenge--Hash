dca2518110d074d9826125cfcff0c67037da4f77
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-05-13 23:05:51 -0500

    ex1: completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,17 @@
 def get_indices_of_item_weights(weights, length, limit):
 
-    """
-    YOUR CODE HERE
-    """
+    w_dictionary = {}
+    for k, v in enumerate(weights):
+        if v in w_dictionary:
+            w_dictionary[v].append(k)
+        else:
+            w_dictionary[v] = [k]
+    for i, w in enumerate(w_dictionary):
+        if limit-w in w_dictionary:
+            if w_dictionary[limit-w] is w_dictionary[w]:
+                if len(w_dictionary[w]) >= 2:
+                    return (w_dictionary[limit-w][1], w_dictionary[w][0])
+            else:
+                return (w_dictionary[limit-w][0], w_dictionary[w][0])
 
     return None
